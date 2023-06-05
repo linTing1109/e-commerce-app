e55230e6b7fce1a9313c0ad482e98e639920e9e6
linTing1109 <tina771109@gmail.com>
2023-06-06 02:51:04 +0800

    20230606-專題版

--- src/Homework/MemberAdd.js
+++ src/Homework/MemberAdd.js
@@ -11,6 +11,9 @@ import Button from 'react-bootstrap/Button';
 // 會員新增 api
 const apiUrl = 'http://localhost:8090/training/ecommerce/MemberController/addMemberLogin';
 
+// 寄送信件 api
+const apiUrlMail = 'http://localhost:8090/training/ecommerce/MailController/sendMailText';
+
 const MemberAdd = () => {
   // 遮罩使用
   const [isLoading, setIsLoading] = useState(false);
@@ -26,7 +29,7 @@ const MemberAdd = () => {
         cusPassword: '',
         cusName:'',
     },
-
+    mail:'',
     //回傳所需的資料 密碼不會秀出來 所以這邊不會有密碼
     idNoUpdate: '',
     cusNameUpdate: '',
@@ -36,7 +39,7 @@ const MemberAdd = () => {
 });
 
   // 解構 
-  const {memberInfoVo,idNoUpdate,cusNameUpdate,loginMessage,isLogin} =data ;
+  const {memberInfoVo,idNoUpdate,cusNameUpdate,loginMessage,isLogin,mail} =data ;
   const {identificationNo,cusPassword,cusName}=memberInfoVo;
 
   const changeMemberID = (event) => {
@@ -66,6 +69,13 @@ const MemberAdd = () => {
       },
     }));
   };
+
+  const changeCusMail = (event) => {
+    setData((prevState) => ({
+      ...prevState,
+      mail:event.target.value,
+    }));
+  };  
   
   // 點擊登入按鈕  
   const handleSubmit = async(event)  => {
@@ -90,16 +100,55 @@ const MemberAdd = () => {
                 loginMessage: memberData.loginMessage,
                 isLogin:memberData.isLogin,
           }));
+          //下面這邊是寄送註冊成功 或註冊失敗信件
+          // const fetchList2 = async() => {
+            //注意:由於信件內容純文字格式 所以用的是\n 而不是<br/>
+           
+           const mailRealText = memberData.isLogin ?`歡迎註冊E-Commerce-App\n帳號名稱: ${memberInfoVo.identificationNo}\n用戶名稱: ${memberInfoVo.cusName}\n用戶密碼: ${memberInfoVo.cusPassword}密碼請託善保管`:
+                                        `歡迎註冊E-Commerce-App\n帳號名稱: ${memberInfoVo.identificationNo}帳號已存在請重新確認`;
+           
+           const mailTitle= memberData.isLogin ? `會員註冊成功`:`會員註冊失敗`;
+           
+            const params = {// 固定送到客服信箱內
+                "mailAddress": mail, "message": mailTitle, "messageText": mailRealText
+            };
+            const mailResult = await axios.get(apiUrlMail, {params},{ withCredentials: true })
+                .then(rs => rs.data)
+                .catch(error => { console.log(error); });
+                setData((prevState) => ({
+                  ...prevState,
+                  mailResult:mailResult,
+                  }));
+            // };
 
             // 資料新增成功 導入會員登入頁
               setIsLoading(true);
-              setTimeout(() => {
+              setTimeout(async() => {
                 setIsLoading(false);
-                if(isLogin==true){
-                  navigate('/'); //若成功登入則導頁到會員登入頁面
+                if(isLogin){//新增成功導入會員登入
+                  navigate('/'); 
+                }else{//註冊失敗也導入會員登入
+                  navigate('/'); 
                 }
               }, 1000); // 1秒後自動關閉
 };
+
+const fetchList2 = async() => {
+  //注意:由於信件內容純文字格式 所以用的是\n 而不是<br/>
+  const mailRealText = `歡迎註冊E-Commerce-App\n帳號名稱: ${memberInfoVo.identificationNo}\n用戶名稱: ${memberInfoVo.cusName}\n用戶密碼: ${memberInfoVo.cusPassword}密碼請託善保管`;
+
+  const params = {// 固定送到客服信箱內
+      "mailAddress": mail, "message": "會員註冊成功", "messageText": mailRealText
+  };
+  const mailResult = await axios.get(apiUrlMail, {params},{ withCredentials: true })
+      .then(rs => rs.data)
+      .catch(error => { console.log(error); });
+      setData((prevState) => ({
+        ...prevState,
+        mailResult:mailResult,
+        }));
+  };
+
 
   return (
     <div>
@@ -137,6 +186,18 @@ const MemberAdd = () => {
                         <FormControl
                         type="text" name="cusName" placeholder="Enter cusName" required  
                         onBlur={changeCusName}  // 使用 onBlur 避免頻繁被觸發
+                        />
+
+                        <Form.Control.Feedback type="invalid">尚未輸入!</Form.Control.Feedback>
+                    </Form.Group>
+                </Col>
+
+                <Col xs={3}>
+                    <Form.Group >
+                        <Form.Label>會員信箱 mail</Form.Label>
+                        <FormControl
+                        type="text" name="cusMail" placeholder="Enter cusMail" required  
+                        onBlur={changeCusMail}  // 使用 onBlur 避免頻繁被觸發
                         />
 
                         <Form.Control.Feedback type="invalid">尚未輸入!</Form.Control.Feedback>
